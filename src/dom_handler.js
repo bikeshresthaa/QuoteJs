a98be808b4303cb1e6a5ba56ce5045ba753ee025
bikeshresthaa <eshbikesh@gmail.com>
2025-12-12 17:43:38 +0545

    feat: implement feature for sequential navigation

--- src/dom_handler.js
+++ src/dom_handler.js
@@ -1,49 +1,71 @@
 import { quotesManager } from "./quotes_handler.js";
 
 const initializeEventListeners = function (){
+
+  
+  const handleNext = function(){
+    displayQuote(currentCategoryList.getNextQuote());
+  }
+
+  const handlePrev = function(){
+    displayQuote(currentCategoryList.getPrevQuote());
+  }
+  
+  const randomHandler = function(){
+    displayQuote(currentCategoryList.getRandomQuote());
+  }
+  
+  const fontHandler = function(){
+    let currentFont = this.value;
+    const quote = getQuoteDisplay();
+    quote.style.fontSize = currentFont;
+  }
+  
+  const categoryHandler = function() {
     
-    let fontSelector = document.getElementById('font-select');
-    fontSelector.addEventListener('change', fontHandler);
+    currentCategoryList = quotesManager(this.value);
+    displayQuote(currentCategoryList.getQuote());
+    
+  }
+  
+  const displayQuote = function(content){
+    let quoteDisplay = getQuoteDisplay();
+    quoteDisplay.textContent = "";
+    quoteDisplay.textContent = content;
+  }
+  
+  const getQuoteDisplay = function(){
+    return document.querySelector('.quote');
+  };
+  
+  const getCategory = function(){
+    return document.getElementById('quote-category').value;
+  }
+  
+  const getQuoteList = function(){
+    return quotesManager(getCategory());
+  }
 
-    let categorySelector = document.getElementById('quote-category');
-    categorySelector.addEventListener('change', categoryHandler);
+  let currentCategoryList = getQuoteList();
+    
+  const fontSelector = document.getElementById('font-select');
+  fontSelector.addEventListener('change', fontHandler);
 
-    categorySelector.dispatchEvent(new Event('change'));
-    fontSelector.dispatchEvent(new Event('change'));
+  const categorySelector = document.getElementById('quote-category');
+  categorySelector.addEventListener('change', categoryHandler);
 
-    let randomBtn = document.getElementById('random-quote-btn');
-    randomBtn.addEventListener('click', randomHandler);
+  categorySelector.dispatchEvent(new Event('change'));
+  fontSelector.dispatchEvent(new Event('change'));
 
+  const randomBtn = document.getElementById('random-quote-btn');
+  randomBtn.addEventListener('click', randomHandler);
+
+  const nextBtn = document.getElementById('next');
+  nextBtn.addEventListener('click', handleNext);
+
+  const prevBtn = document.getElementById('prev');
+  prevBtn.addEventListener('click', handlePrev);
 }
 
-const randomHandler = function(){
-  let selectedCategory = document.getElementById('quote-category').value;
-  let quoteList = quotesManager(selectedCategory);
-  displayQuote(quoteList.getRandomQuote());
-
-}
-
-const fontHandler = function(){
-  let currentFont = this.value;
-  const quote = getQuoteDisplay();
-  quote.style.fontSize = currentFont;
-}
-
-const categoryHandler = function() {
-  let currentCategory = this.value;
-  const quoteList = quotesManager(currentCategory);
-  displayQuote(quoteList.getQuote());
-
-}
-
-const displayQuote = function(content){
-  let quoteDisplay = getQuoteDisplay();
-  quoteDisplay.textContent = "";
-  quoteDisplay.textContent = content;
-}
-
-const getQuoteDisplay = function(){
-  return document.querySelector('.quote');
-};
 
 export { initializeEventListeners };
